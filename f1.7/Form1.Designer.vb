73d61aaea7da33db2d8044443e07fe3b42bc9144
shamin <Administrador@LAPTOP-6G1LPHN0>
2025-11-26 09:21:13 +0000

    base

--- f1.7/Form1.Designer.vb
+++ f1.7/Form1.Designer.vb
@@ -22,31 +22,120 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        CheckBox1 = New CheckBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
         SuspendLayout()
         ' 
-        ' CheckBox1
+        ' Button1
         ' 
-        CheckBox1.AutoSize = True
-        CheckBox1.Location = New Point(214, 189)
-        CheckBox1.Name = "CheckBox1"
-        CheckBox1.Size = New Size(85, 19)
-        CheckBox1.TabIndex = 0
-        CheckBox1.Text = "CheckBox1"
-        CheckBox1.UseVisualStyleBackColor = True
+        Button1.Location = New Point(38, 49)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(74, 35)
+        Button1.TabIndex = 0
+        Button1.Text = "Jogar"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(38, 107)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(74, 35)
+        Button2.TabIndex = 1
+        Button2.Text = "Sair"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(169, 52)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(66, 23)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(342, 52)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(66, 23)
+        TextBox2.TabIndex = 3
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(256, 52)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(66, 23)
+        TextBox3.TabIndex = 4
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(38, 164)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(54, 15)
+        Label1.TabIndex = 5
+        Label1.Text = "Créditos:"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(48, 196)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(31, 15)
+        Label2.TabIndex = 6
+        Label2.Text = "5000"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(12, 9)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(32, 15)
+        Label3.TabIndex = 7
+        Label3.Text = "Jogo"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(54, 9)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(38, 15)
+        Label4.TabIndex = 8
+        Label4.Text = "Ajuda"
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
-        Controls.Add(CheckBox1)
+        ClientSize = New Size(448, 244)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
         Name = "Form1"
         Text = "Form1"
         ResumeLayout(False)
         PerformLayout()
     End Sub
 
-    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
 
 End Class
--- f1.7/Form1.vb
+++ f1.7/Form1.vb
@@ -1,3 +1,13 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
 End Class
